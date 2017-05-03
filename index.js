8a8ca111d8cf073b1a61ede2d2a74fe52575cf7f
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-05-02 19:02:46 -0500

    New: Initial implementation

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var through = require('through2');
+
+function forward(chunk, enc, cb) {
+  cb(null, chunk);
+}
+
+function toThrough(readable) {
+
+  var opts = {
+    objectMode: readable._readableState.objectMode,
+    highWaterMark: readable._readableState.highWaterMark,
+  };
+
+  function flush(cb) {
+    var self = this;
+
+    readable.on('readable', onReadable);
+    readable.on('end', cb);
+
+    function onReadable() {
+      var chunk;
+      while (chunk = readable.read()) {
+        self.push(chunk);
+      }
+    }
+  }
+
+  var wrapper = through(opts, forward, flush);
+
+  var shouldFlow = true;
+  wrapper.on('pipe', disableFlow);
+
+  function disableFlow() {
+    // If the wrapper is piped, disable flow
+    shouldFlow = false;
+  }
+
+  // Check if we should flow if not piped by nextTick
+  process.nextTick(maybeFlow);
+
+  function maybeFlow() {
+    if (shouldFlow) {
+      // Triggers flush
+      wrapper.end();
+    }
+  }
+
+  return wrapper;
+}
+
+module.exports = toThrough;
---
+++ test/index.js
@@ -0,0 +1,174 @@
+'use strict';
+
+var expect = require('expect');
+
+var miss = require('mississippi');
+
+var toThrough = require('../');
+
+var from = miss.from;
+var pipe = miss.pipe;
+var concat = miss.concat;
+
+describe('toThrough (buffer mode)', function() {
+
+  // These tests ensure it automatically detects buffer mode
+
+  var preContents = ['from', ' ', 'upstream', ' '];
+  var contents = ['hello', ' ', 'world', ' ', '123'];
+
+  it('can wrap a Readable and be used as a Readable', function(done) {
+    var readable = from(contents);
+
+    function assert(result) {
+      expect(result).toEqual(contents.join(''));
+    }
+
+    pipe([
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('can wrap a Readable and be used as a Transform', function(done) {
+    var readable = from(contents);
+
+    function assert(result) {
+      expect(result).toEqual(contents.join(''));
+    }
+
+    pipe([
+      from([]),
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('passes through all upstream before readable', function(done) {
+    var readable = from(contents);
+
+    function assert(result) {
+      expect(result).toEqual(preContents.concat(contents).join(''));
+    }
+
+    pipe([
+      from(preContents),
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('flushes the stream if not piped before nextTick', function(done) {
+    var readable = from(contents);
+
+    var wrapped = toThrough(readable);
+
+    function assert(err) {
+      expect(err).toExist();
+      expect(err.message).toEqual('write after end');
+      done();
+    }
+
+    process.nextTick(function() {
+      pipe([
+        from(preContents),
+        wrapped,
+        concat(assert),
+      ], assert);
+    });
+  });
+});
+
+describe('toThrough (object mode)', function() {
+
+  // These tests ensure it automatically detects objectMode
+
+  var preContents = [
+    { value: -2 },
+    { value: -1 },
+    { value: 0 },
+  ];
+  var contents = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+    { value: 5 },
+    { value: 6 },
+    { value: 7 },
+    { value: 8 },
+    { value: 9 },
+    { value: 10 },
+    { value: 11 },
+    { value: 12 },
+    { value: 13 },
+    { value: 14 },
+    { value: 15 },
+    { value: 16 },
+    { value: 17 },
+    { value: 18 },
+    { value: 19 },
+    { value: 20 },
+  ];
+
+  it('can wrap a Readable and be used as a Readable', function(done) {
+    var readable = from.obj(contents);
+
+    function assert(result) {
+      expect(result).toEqual(contents);
+    }
+
+    pipe([
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('can wrap a Readable and be used as a Transform', function(done) {
+    var readable = from.obj(contents);
+
+    function assert(result) {
+      expect(result).toEqual(contents);
+    }
+
+    pipe([
+      from.obj([]),
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('passes through all upstream before readable', function(done) {
+    var readable = from.obj(contents);
+
+    function assert(result) {
+      expect(result).toEqual(preContents.concat(contents));
+    }
+
+    pipe([
+      from.obj(preContents),
+      toThrough(readable),
+      concat(assert),
+    ], done);
+  });
+
+  it('flushes the stream if not piped before nextTick', function(done) {
+    var readable = from.obj(contents);
+
+    var wrapped = toThrough(readable);
+
+    function assert(err) {
+      expect(err).toExist();
+      expect(err.message).toEqual('write after end');
+      done();
+    }
+
+    process.nextTick(function() {
+      pipe([
+        from.obj(preContents),
+        wrapped,
+        concat(assert),
+      ], assert);
+    });
+  });
+});
